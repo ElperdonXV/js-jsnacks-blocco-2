4867cd2048f1e20267413bc8e24d1442acb037a5
ElperdonXV <davidemartinografico@gmail.com>
2021-11-30 10:09:48 +0100

    all done

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -3,5 +3,9 @@
 const num = ['2', '4', '6', '8', '10', '12'];
 let somma = parseInt(0);
 for (let i=0; i<num.length; i++){
-    
+    if(i % 2 != 0){
+        num[i] = parseInt(num[i]);
+        somma = somma + num[i];
+    }
 }
+console.log(somma);
