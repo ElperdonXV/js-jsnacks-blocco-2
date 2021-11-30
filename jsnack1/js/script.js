81cca093c643e5609155f72f5fa63c4cf9a47183
ElperdonXV <davidemartinografico@gmail.com>
2021-11-30 10:01:41 +0100

    done first snack

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -5,3 +5,14 @@
 const nomi = ['Carmelo', 'Andrea', 'Gianni', 'Davide', 'Gennaro', 'Michele', 'Sara', 'Anna', 'Rosita'];
 const cognomi = ['De Martino', 'Di Lucca', 'Di Francesco', 'Cundari', 'Pennisi', 'Testina', 'Snack', 'John', 'Depp'];
 let invitati = [];
+function randomRangeNumber(number) {
+    number = parseInt(Math.floor(Math.random() * (max - min) + min));
+    return number;
+}
+const min = 0;
+const max = 9;
+for (let i=0; i<9; i++){
+    let randomPull = randomRangeNumber(min, max);
+    invitati.push(nomi[i] + ' ' + cognomi[randomPull]);
+}
+console.log(invitati);
