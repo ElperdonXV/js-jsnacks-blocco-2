5c1f2caf9de911cd6ac5a0f9fddb9bd025e03812
ElperdonXV <davidemartinografico@gmail.com>
2021-11-30 10:05:42 +0100

    work on snack 2

--- jsnack2/js/script.js
+++ jsnack2/js/script.js
@@ -1,2 +1,7 @@
 //Crea un array di numeri interi e fai la somma 
 //di tutti gli elementi che sono in posizione dispari
+const num = ['2', '4', '6', '8', '10', '12'];
+let somma = parseInt(0);
+for (let i=0; i<num.length; i++){
+    
+}
