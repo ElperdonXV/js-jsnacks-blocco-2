9a252aa23ffec82a9ec2c78a1c62c515ea4219fa
ElperdonXV <davidemartinografico@gmail.com>
2021-11-30 09:54:29 +0100

    work on array

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,3 +1,7 @@
 //Generatore di “nomi cognomi” casuali: 
 //prendendo una lista di nomi e una lista di cognomi, 
 //Gatsby vuole generare una falsa lista di invitati.
+
+const nomi = ['Carmelo', 'Andrea', 'Gianni', 'Davide', 'Gennaro', 'Michele', 'Sara', 'Anna', 'Rosita'];
+const cognomi = ['De Martino', 'Di Lucca', 'Di Francesco', 'Cundari', 'Pennisi', 'Testina', 'Snack', 'John', 'Depp'];
+let invitati = [];
